aae95e204f88d059bb73c961bdc9548b8ce60652
iBinh <ntbinh21@gmail.com>
2020-10-27 14:03:14 +0700

    update

--- DerivedData/GoongDirections/Build/Intermediates.noindex/GoongDirections.build/Debug-iphoneos/GoongDirections.build/DerivedSources/GoongDirections_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char GoongDirectionsVersionString[];
- extern const double GoongDirectionsVersionNumber;
-
- const unsigned char GoongDirectionsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:GoongDirections  PROJECT:GoongDirections-1" "\n";
- const double GoongDirectionsVersionNumber __attribute__ ((used)) = (double)1.;
